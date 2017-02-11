77ce0e9abadbcb9a38e6945cb8a171b931faa51e
John Turner <john.turner@monkeylittle.com>
2017-02-11 23:17:13 +0000

    Implement initial support for Tag index pages and Tag linking.

--- _plugins/tag_page_generator.rb
+++ _plugins/tag_page_generator.rb
@@ -1,58 +1,46 @@
 module Jekyll
 
-  class TagPageGenerator < Generator
-    safe true
+  class TagPage < Jekyll::Page
 
-    def generate(site)
-      if site.layouts.key? 'tag_index'
-        dir = site.config['tag_dir'] || 'tags'
-
-        site.tags.each_key do |tag|
-          tag_posts = site.tags[tag]
-          tag_page_count = TagPager.calculate_pages(tag_posts, site.config['paginate'].to_i)
-
-          (1..tag_page_count).each do |page_number|
-            tag_pager = TagPager.new(site, page_number, tag_posts, tag, tag_page_count)
-            tag_dir = File.join('tags', tag, page_number > 1 ? "page#{page_number}" : '')
-            tag_page = TagPage.new(site, site.source, tag_dir, tag)
-            tag_page.pager = tag_pager
-            site.pages << tag_page
-          end
-        end
-
-      end
-    end
-  end
-
-  class TagPage < Page
     def initialize(site, base, dir, tag)
+
       @site = site
       @base = base
       @dir = dir
       @name = 'index.html'
 
-      layout = site.config['tag_index']['layout'] || 'tag_index.html'
-
       self.process(@name)
-      self.read_yaml(File.join(base, '_layouts'), layout)
+      self.read_yaml(File.join(base, '_layouts'), 'tag_index.html')
       self.data['tag'] = tag
     end
+
   end
 
-  class TagPager < Jekyll::Paginate::Pager
-    attr_reader :tag
+  class TagPageGenerator < Jekyll::Generator
+    safe true
 
-    def initialize(site, page, all_posts, tag, num_pages = nil)
-      @tag = tag
-      super site, page, all_posts, num_pages
-    end
+    def generate(site)
 
-    alias_method :original_to_liquid, :to_liquid
+      if site.config['tag']
+        site.tags.each_key do |tag|
 
-    def to_liquid
-      liquid = original_to_liquid
-      liquid['tag'] = @tag
-      liquid
+          posts = site.tags[tag]
+          page_count = Jekyll::Paginate::Pager.calculate_pages(posts, site.config['paginate'].to_i)
+
+          (1..page_count).each do |page_number|
+
+            tag_dir = File.join('tags', tag, page_number > 1 ? "page#{page_number}" : '')
+
+            pager = Jekyll::Paginate::Pager.new(site, page_number, posts, page_count)
+
+            page = TagPage.new(site, site.source, tag_dir, tag)
+            page.data['tag'] = tag
+            page.pager = pager
+            site.pages << page
+          end
+        end
+      end
+
     end
   end
 
